32462a4b0ca3ae499497ae954e0e54e72b957436
Rui <rwang778@gmail.com>
2021-02-26 13:12:00 -0800

    Update greyscale.py

--- PyGram/greyscale.py
+++ PyGram/greyscale.py
@@ -1,7 +1,7 @@
-def greyscale(input_path, output_path):
+def greyscale(image, output_path):
     '''
     Converting a color image into a greyscale image
-    Input: input_path: string, path for the input image file
+    Input: image: string, path for the input image file
            output_path: string, path for the output image file
            
     Output: an image file at the specified output path
