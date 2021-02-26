7285de23c123211c31aae6fcc01c4ba511de7e6e
Rui Wang <rwang778@student.ubc.ca>
2021-02-26 13:09:16 -0800

    Add function specifications

---
+++ PyGram/rotate.py
@@ -0,0 +1,11 @@
+# This script is for function rotate
+
+def rotate(image, degree, output_path):
+    '''
+    Rotate an image by a given degree
+    Arguments:
+        image: path of input file
+        degree: int of degree
+    Output: an image file in .png format
+    '''
+    pass
