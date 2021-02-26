6ae59fcefa998f8d2c6223c92ad33dc6a4508c27
jla510 <jla510@student.ubc.ca>
2021-02-26 13:06:47 -0800

    Added function specifications for flipping ad greyscale function

---
+++ PyGram/greyscale.py
@@ -0,0 +1,9 @@
+def greyscale(input_path, output_path):
+    '''
+    Converting a color image into a greyscale image
+    Input: input_path: string, path for the input image file
+           output_path: string, path for the output image file
+           
+    Output: an image file at the specified output path
+    '''
+    pass
