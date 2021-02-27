22e33d55aaf6fb1ea182696b3ac6a1e95885b281
Rui <rwang778@gmail.com>
2021-02-27 10:04:45 -0800

    Update flipping.py

--- PyGram/flipping.py
+++ PyGram/flipping.py
@@ -3,8 +3,11 @@ def flipping(image, direction,output_path):
     Flipping an image in either horizontal or vertical direction
     
     Arguments:
+    -----------------------------
         image: path of the input file
         direction: direction of flip, horizontal="h", vertical = "v"
-    Output: an image file in .png format
+    Output:
+    -----------------------------
+        an image file in .png format
     '''
     pass
