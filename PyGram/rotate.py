6fbe6435143a7204663d37319a19f94c83b5d0ee
jla510 <jla510@student.ubc.ca>
2021-03-05 01:55:21 -0800

    Update function

--- PyGram/rotate.py
+++ PyGram/rotate.py
@@ -1,7 +1,7 @@
 # This script is for the rotate function
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+import cv2
 
 
 def rotate(image, degree, output_path):
@@ -36,42 +36,62 @@ def rotate(image, degree, output_path):
         print(e)
         raise
 
-    # Adapted from stackoverflow
-    # convert rotation amount to radian
-    rotation_amount_rad = degree * np.pi / 180.0
-    # get dimension info
-    height, width, num_channels = image.shape
-    # create output image, for worst case size (45 degree)
-    max_len = int(math.sqrt(height * height + width * width))
-    rotated_image = np.zeros((max_len, max_len, num_channels))
-    rotated_height, rotated_width, _ = rotated_image.shape
-    mid_row = int((rotated_height + 1) / 2)
-    mid_col = int((rotated_width + 1) / 2)
+    # Get the image size
+    image_size = (image.shape[1], image.shape[0])
+    image_center = tuple(np.array(image_size) / 2)
 
-    for r in range(rotated_height):
-        for c in range(rotated_width):
-            #  apply rotation matrix, the other way
-            y = (r - mid_col) * math.cos(rotation_amount_rad) + (
-                c - mid_row
-            ) * math.sin(rotation_amount_rad)
-            x = -(r - mid_col) * math.sin(rotation_amount_rad) + (
-                c - mid_row
-            ) * math.cos(rotation_amount_rad)
+    # Convert the OpenCV 3x2 rotation matrix to 3x3
+    rot_mat = np.vstack([cv2.getRotationMatrix2D(image_center, degree, 1.0), [0, 0, 1]])
 
-            #  add offset
-            y += mid_col
-            x += mid_row
+    rot_mat_notranslate = np.matrix(rot_mat[0:2, 0:2])
 
-            #  get nearest index, a better way is linear interpolation
-            x = round(x)
-            y = round(y)
-            #  check if x/y corresponds to a valid pixel in input image
-            if x >= 0 and y >= 0 and x < width and y < height:
-                rotated_image[r][c][:] = image[y][x][:]
+    # Shorthand for below calcs
+    image_w2 = image_size[0] * 0.5
+    image_h2 = image_size[1] * 0.5
+
+    # Obtain the rotated coordinates of the image corners
+    rotated_coords = [
+        (np.array([-image_w2, image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([image_w2, image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([-image_w2, -image_h2]) * rot_mat_notranslate).A[0],
+        (np.array([image_w2, -image_h2]) * rot_mat_notranslate).A[0],
+    ]
+
+    # Find the size of the new image
+    x_coords = [pt[0] for pt in rotated_coords]
+    x_pos = [x for x in x_coords if x > 0]
+    x_neg = [x for x in x_coords if x < 0]
+
+    y_coords = [pt[1] for pt in rotated_coords]
+    y_pos = [y for y in y_coords if y > 0]
+    y_neg = [y for y in y_coords if y < 0]
+
+    right_bound = max(x_pos)
+    left_bound = min(x_neg)
+    top_bound = max(y_pos)
+    bot_bound = min(y_neg)
+
+    new_w = int(abs(right_bound - left_bound))
+    new_h = int(abs(top_bound - bot_bound))
+
+    # We require a translation matrix to keep the image centred
+    trans_mat = np.matrix(
+        [
+            [1, 0, int(new_w * 0.5 - image_w2)],
+            [0, 1, int(new_h * 0.5 - image_h2)],
+            [0, 0, 1],
+        ]
+    )
+
+    # Compute the tranform for the combined rotation and translation
+    affine_mat = (np.matrix(trans_mat) * np.matrix(rot_mat))[0:2, :]
+
+    # Apply the transform
+    result = cv2.warpAffine(image, affine_mat, (new_w, new_h), flags=cv2.INTER_LINEAR)
 
     # exception handling
     try:
-        plt.imshow(rotated_image)
+        plt.imshow(result)
         plt.savefig(output_path)
     except FileNotFoundError:
         print("The output path does not exist.")
