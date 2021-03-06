a67367992296e2f599823b62a37aba679932c473
Rui Wang <rwang778@student.ubc.ca>
2021-03-06 12:20:36 -0800

    update path names and add system paths

--- tests/test_flipping.py
+++ tests/test_flipping.py
@@ -5,23 +5,26 @@ import pytest
 import numpy as np
 from imageprocessor import flipping
 
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
 def test_flippingH():
     '''
     Check if the horizontal flipping function 
     '''
-    flipping("pygram/tests/images/sample.png", "h", "pygram/tests/images/sample_flipping_h.png")
-    output = skimage.io.imread("pygram/tests/images/samples_flipping_temp.png")
-    test_output = skimage.io.imread("pygram/tests/images/sample_flipping_h.jpg")
+    flipping("imageprocessor/tests/images/sample.png", "h", "imageprocessor/tests/images/sample_flipping_h.png")
+    output = skimage.io.imread("imageprocessor/tests/images/samples_flipping_temp.png")
+    test_output = skimage.io.imread("imageprocessor/tests/images/sample_flipping_h.jpg")
     assert np.array_equal(output, test_output), "The flipping function does not work properly"   
 
 def test_flippingV():
     '''
     Testing the the vertical flipping function
     '''
-    flipping("pygram/tests/images/sample.png", "v", "pygram/tests/images/sample_flipping_v.png")
-    output = skimage.io.imread("pygram/tests/images/sample_flipping_temp.png")
-    test_output = skimage.io.imread("pygram/tests/images/sample_flipping_v.jpg")
+    flipping("imageprocessor/tests/images/sample.png", "v", "imageprocessor/tests/images/sample_flipping_v.png")
+    output = skimage.io.imread("imageprocessor/tests/images/sample_flipping_temp.png")
+    test_output = skimage.io.imread("imageprocessor/tests/images/sample_flipping_v.jpg")
     assert np.array_equal(output, test_output), "The flipping function does not work properly"
 
 
@@ -34,7 +37,7 @@ def test_non_string_input():
     '''    
     
     with pytest.raises(AttributeError):
-        flipping(555, "h", "pygram/tests/images/sample.png")
+        flipping(555, "h", "imageprocessor/tests/images/sample.png")
         
 def test_non_string_output():
     
@@ -42,7 +45,7 @@ def test_non_string_output():
     Check unexpected flipping function for output path format 
     '''
     with pytest.raises(AttributeError):
-        flipping("pygram/tests/images/sample.png", "v", 555)
+        flipping("imageprocessor/tests/images/sample.png", "v", 555)
 
 def test_nonexistent_input_path():
     
@@ -50,7 +53,7 @@ def test_nonexistent_input_path():
     Check unexpected flipping function for input path
     '''
     with pytest.raises(FileNotFoundError):
-        flipping("./555/456.png", "v", "pygram/tests/images/sample.png")
+        flipping("./555/456.png", "v", "imageprocessor/tests/images/sample.png")
 
 def test_nonexistent_output_path():
     
@@ -58,4 +61,6 @@ def test_nonexistent_output_path():
     Check unexpected flipping function input for output
     '''
     with pytest.raises(FileNotFoundError):
-        flipping("pygram/tests/images/samples.png", "h", "./555/456.jpg")
+        flipping("imageprocessor/tests/images/samples.png", "h", "./555/456.jpg")
+
+test_flippingH()
--- tests/test_greyscale.py
+++ tests/test_greyscale.py
@@ -1,17 +1,18 @@
 import os, sys
 import matplotlib.pyplot as plt
-#currentdir = os.path.dirname(os.path.realpath(__file__))
-#parentdir = os.path.dirname(currentdir)
-#sys.path.append(parentdir)
 from imageprocessor import greyscale
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
 
 def test_greyscale():
     '''
     Check if the greyscale function could get the expected image
     '''
-    greyscale("pygram/tests/images/sample.png", "pygram/tests/images/samples_temp.png")
-    output = skimage.io.imread("pygram/tests/images/samples_greyscale_temp.png")
-    test_output = skimage.io.imread("pygram/tests/images/sample_greyscale.png")
+    greyscale("imageprocessor/tests/images/sample.png", "imageprocessor/tests/images/samples_temp.png")
+    output = skimage.io.imread("imageprocessor/tests/images/samples_greyscale_temp.png")
+    test_output = skimage.io.imread("imageprocessor/tests/images/sample_greyscale.png")
     assert np.array_equal(output, test_output), "The greyscale function does not work properly"
 
 #Exception Handling
@@ -20,25 +21,25 @@ def test_non_string_input():
     Check unexpected greyscale function input
     '''
     with pytest.raises(AttributeError):
-        greyscale(123, "pygram/tests/images/samples.png")
+        greyscale(123, "imageprocessor/tests/images/samples.png")
 
 def test_non_string_output():
     '''
     Check unexpected greyscale function for output path format 
     '''
     with pytest.raises(AttributeError):
-        greyscale("pygram/tests/images/samples.png", 123)
+        greyscale("imageprocessor/tests/images/samples.png", 123)
 
 def test_nonexistent_input_path():
     '''
     Check unexpected greyscale function for input path
     '''
     with pytest.raises(FileNotFoundError):
-        greyscale("./123/456.png", "pygram/tests/images/samples.png")
+        greyscale("./123/456.png", "imageprocessor/tests/images/samples.png")
 
 def test_nonexistent_output_path():
     '''
     Check unexpected greyscale function input for output
     '''
     with pytest.raises(FileNotFoundError):
-        greyscale("pygram/tests/images/samples.png", "./123/456.jpg")
+        greyscale("imageprocessor/tests/images/samples.png", "./123/456.jpg")
