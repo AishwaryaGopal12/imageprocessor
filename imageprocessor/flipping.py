9638c01eb55b60b7696069d74d3e1b9fdfc36c9b
Mo Garoub <58850257+mgaroub@users.noreply.github.com>
2021-03-06 11:58:39 -0800

    Update flipping.py

--- imageprocessor/flipping.py
+++ imageprocessor/flipping.py
@@ -1,4 +1,9 @@
-def flipping(image, direction,output_path):
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage.io
+
+
+def flipping(image, direction, output_path):
     '''
     Flipping an image in either horizontal or vertical direction
     
@@ -10,4 +15,55 @@ def flipping(image, direction,output_path):
     -----------------------------
         an image file in .png format
     '''
-    pass
+    
+    assert direction in ["h","v"], "Invalid flipping direction"
+# exception handling
+
+    try:
+        input_image = plt.imread(image)
+        print("Success")
+    except FileNotFoundError:
+        print("The input file/path does not exist")
+    except OSError:
+        print("The input file is not an image.")
+    except AttributeError:
+        print("Please type in  a string as the path for the input image file.")
+    except Exception as e:
+        print("Other Errors")
+        print(e)
+        raise
+
+    
+#Horizontal Flipping
+    if direction == "h":
+        original=range(0, input_image.shape[1])
+        flipped=list(reversed(original))
+        output_matrix = input_image.copy()
+        output_matrix[:,original] = input_image[:,flipped]
+
+#Vertical Flipping
+
+    elif direction == "v":
+        original=range(0, input_image.shape[0])
+        flipped=list(reversed(original))
+        output_matrix = input_image.copy()
+        output_matrix[original] = input_image[flipped]
+
+
+    #Data.type
+    output_matrix=np.array(output_matrix, dtype=np.uint8)
+
+
+    # exception handling
+    try:
+
+        plt.imshow(input_image)
+        plt.savefig(output_path)
+    except FileNotFoundError:
+        print("The output path does not exist.")
+    except AttributeError:
+        print("Please provide a string as the path for the output image file.")
+    except Exception as e:
+        print("Other exceptions, please check your input and output. ")
+        print(e)
+        raise
