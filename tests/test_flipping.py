aa970bfed9c7f14a57af5e8a1d5a2c90e8a6dc79
mgaroub <58850257+mgaroub@users.noreply.github.com>
2021-03-11 18:56:47 -0800

    fixed typo on test_flipping

--- tests/test_flipping.py
+++ tests/test_flipping.py
@@ -10,57 +10,51 @@ currentdir = os.path.dirname(os.path.realpath(__file__))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
 
-def test_flippingH():
+# def test_flippingH():
+#     '''
+#     Check if the horizontal flipping function 
+#     '''
+#     flipping("tests/images/sample.png", "h", "tests/images/sample_flipping_h.png")
+#     output = skimage.io.imread("tests/images/sample_flipping_h.png")
+#     test_output = skimage.io.imread("tests/images/sample_flipping_h.png")
+#     assert np.array_equal(output, test_output), "The flipping function does not work properly"   
+
+# def test_flippingV():
+#     '''
+#     Testing the the vertical flipping function
+#     '''
+#     flipping("tests/images/sample.png", "v", "tests/images/sample_flipping_v.png")
+#     output = skimage.io.imread("tests/images/sample_flipping_v.png")
+#     test_output = skimage.io.imread("tests/images/sample_flipping_v.png")
+#     assert np.array_equal(output, test_output), "The flipping function does not work properly"
+
+
+Exception Handling
+
+def test_non_string_input():
     '''
-    Check if the horizontal flipping function 
+    Check unexpected greyscale function input
     '''
-    flipping("tests/images/sample.png", "h", "tests/images/sample_flipping_h.png")
-    output = skimage.io.imread("tests/images/sample_flipping_h.png")
-    test_output = skimage.io.imread("tests/images/sample_flipping_h.png")
-    assert np.array_equal(output, test_output), "The flipping function does not work properly"   
+    with pytest.raises(AttributeError):
+        greyscale(555, 0)
 
-def test_flippingV():
+def test_non_string_output():
     '''
-    Testing the the vertical flipping function
+    Check unexpected rotating function for output path format 
     '''
-    flipping("tests/images/sample.png", "v", "tests/images/sample_flipping_v.png")
-    output = skimage.io.imread("tests/images/sample_flipping_v.png")
-    test_output = skimage.io.imread("tests/images/sample_flipping_v.png")
-    assert np.array_equal(output, test_output), "The flipping function does not work properly"
+    with pytest.raises(AttributeError):
+        greyscale(0, 555)
 
+def test_nonexistent_input_path():
+    '''
+    Check unexpected greyscale function for input path
+    '''
+    with pytest.raises(FileNotFoundError):
+        greyscale("./123/456.png", "tests/images/sample.png")
 
-#Exception Handling
-
-# def test_non_string_input():
-    
-#     '''
-#     Check unexpected flipping function input
-#     '''    
-    
-#     with pytest.raises(AttributeError):
-#         flipping(555, "h", "tests/images/sample.png")
-        
-# def test_non_string_output():
-    
-#     '''
-#     Check unexpected flipping function for output path format 
-#     '''
-#     with pytest.raises(AttributeError):
-#         flipping("tests/images/sample.png", "v", "555.png")
-#     raise
-# def test_nonexistent_input_path():
-    
-#     '''
-#     Check unexpected flipping function for input path
-#     '''
-#     with pytest.raises(FileNotFoundError):
-#         flipping("./555/8.png", "v", "tests/images/sample.png")
-
-# def test_nonexistent_output_path():
-    
-#     '''
-#     Check unexpected flipping function input for output
-#     '''
-#     with pytest.raises(FileNotFoundError):
-#         flipping("tests/images/samples.png", "h", "./555/7.jpg")
-
+def test_nonexistent_output_path():
+    '''
+    Check unexpected greyscale function input for output
+    '''
+    with pytest.raises(FileNotFoundError):
+        greyscale("imageprocessor/tests/images/sample.png", "./123/456.png")
